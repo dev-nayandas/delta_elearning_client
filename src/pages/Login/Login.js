8b5e80b35dbba07319eaf767575adf5107a268b5
unknown <rich.nayan@gmail.com>
2022-10-26 11:11:07 +0600

    login and register page done

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -3,36 +3,49 @@ import { Link } from 'react-router-dom';
 
 const Login = () => {
     return (
-        <div className="hero min-h-screen bg-base-200">
-  <div className="hero-content flex-col lg:flex-row-reverse">
-  
-    <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-      <div className="card-body">
-        <div className="form-control">
-          <label className="label">
-            <span className="label-text">Email</span>
-          </label>
-          <input type="text" placeholder="email" className="input input-bordered" />
+
+
+
+        
+        <div>
+           
+            <div className="hero min-h-screen bg-base-200">
+              
+              <div className="hero-content flex-col lg:flex-row-reverse">
+              
+                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                  <div className="card-body">
+                    <div className="form-control">
+                      <label className="label">
+                        <span className="label-text">Email</span>
+                      </label>
+                      <input type="text" placeholder="email" className="input input-bordered" />
+                    </div>
+                    <div className="form-control">
+                      <label className="label">
+                        <span className="label-text">Password</span>
+                      </label>
+                      <input type="text" placeholder="password" className="input input-bordered" />
+                      <label className="label">
+                        <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                      </label>
+                      <label className="label">
+                        <p> New to Delta Learning Hub? </p><a href="./register" className="label-text-alt link link-hover">Register Now</a>
+                      </label>
+                    </div>
+                    <div className="form-control mt-6">
+                      <button className="btn btn-primary">Login</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              
+            </div>
+            
+         
+            <button className="btn btn-primary">Google Login</button>  
+            <button className="btn btn-primary">Git Hub Login</button>  
         </div>
-        <div className="form-control">
-          <label className="label">
-            <span className="label-text">Password</span>
-          </label>
-          <input type="text" placeholder="password" className="input input-bordered" />
-          <label className="label">
-            <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-          </label>
-          <label className="label">
-            <p> New to Delta Learning Hub</p><Link href="./register" className="label-text-alt link link-hover">Register Now</Link>
-          </label>
-        </div>
-        <div className="form-control mt-6">
-          <button className="btn btn-primary">Login</button>
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
     );
 };
 
--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -3,7 +3,8 @@ import { Link } from 'react-router-dom';
 
 const Register = () => {
     return (
-        <div className="hero min-h-screen bg-base-200">
+       <div>
+         <div className="hero min-h-screen bg-base-200">
         <div className="hero-content flex-col lg:flex-row-reverse">
           
           <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
@@ -25,11 +26,9 @@ const Register = () => {
                   <span className="label-text">Password</span>
                 </label>
                 <input type="password" placeholder="password" className="input input-bordered" />
+                
                 <label className="label">
-                  <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-                </label>
-                <label className="label">
-                  <p>Already have and account</p>   <Link href="/login" className="label-text-alt link link-hover">Login Now</Link>
+                  <p>Already have and account? </p>   <a href="/login" className="label-text-alt link link-hover">Login Now</a>
                 </label>
               </div>
               <div className="form-control mt-6">
@@ -38,7 +37,12 @@ const Register = () => {
             </div>
           </div>
         </div>
+        
       </div>
+        
+      <button className="btn btn-primary">Google Sign Up</button>  
+            <button className="btn btn-primary">Git Hub Sign Up </button>  
+       </div>
     );
 };
 
